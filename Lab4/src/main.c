21e3e864a9de65ba587484ddf248bc2f9759c017
DESKTOP-6HFM2RN\max <bircumaxim@gmail.com>
2016-11-20 16:18:18 +0200

    Renamed schematics project.

--- Lab4/src/main.c
+++ Lab4/src/main.c
@@ -1,7 +1,7 @@
 /*
- * Lab4.c
+ * src.c
  *
- * Created: 11/11/2016 8:12:49 AM
+ * Created: 11/20/2016 3:59:23 PM
  *  Author: max
  */ 
 
@@ -10,15 +10,15 @@
 #include "Drivers/headers/L293.h"
 
 int main(void)
-{	
+{
 	L293Out l293Out1 = registerL293Out(2, 3, 4, &DDRD, &PORTD);
 	L293Out l293Out2 = registerL293Out(5, 6, 7, &DDRD, &PORTD);
 	
 	L293 l293 = registerL293(l293Out1, l293Out2);
 	
 	
-    while(1)
-    {
+	while(1)
+	{
 		startWheel(l293.out1);
-    }
+	}
 }
